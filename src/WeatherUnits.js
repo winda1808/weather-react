7d8d69a4243824becaeef9de8bb22042d596dbb8
winda1808 <105516942+winda1808@users.noreply.github.com>
2022-07-29 01:04:50 +0800

    removing Fahrenheit conversion

--- src/WeatherUnits.js
+++ src/WeatherUnits.js
@@ -1,45 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import './Weather.css';
 
 
 export default function WeatherUnits(props) {
-    const [unit, setUnit] = useState ("celsius");
-    function showFahrenheit (event) {
-        event.preventDefault ();
-        setUnit("fahrenheit");
-    }
-
-    function showCelsius (event) {
-        event.preventDefault ();
-        setUnit("celsius");
-    }
-            
-
-    if (unit === "celsius") {
+   
     return (
         <div className="Main">
          {Math.round(props.celsius)}  
         <span className="Units">
            <span className="Active">°C</span>
-           <span className="Passive">
-            <a href="/" onClick={showFahrenheit}>|°F</a>
-            </span>
+          
         </span>
         </div>
     );
-} else {
+}
 
-        let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-        <div className="Main">
-         {Math.round(fahrenheit)}  
-        <span className="Units">
-           <span className="Passive">
-           <a href="/" onClick={showCelsius}>°C</a>
-            </span>
-           <span className="Active">|°F</span>
-        </span>
-        </div>
-    );
-}
-}
+
+//from 6
+// const [unit, setUnit] = useState ("celsius");
+//function showFahrenheit (event) {
+   // event.preventDefault ();
+    //setUnit("fahrenheit");}
+
+//function showCelsius (event) {
+    //event.preventDefault ();
+   // setUnit("celsius");}
+        
+// from 17
+//if (unit === "celsius")
+// from 24
+//<span className="Passive">
+// <a href="/" onClick={showFahrenheit}>|°F</a></span>
+// from 27
+// else {
+//let fahrenheit = (props.celsius * 9) / 5 + 32;
+    //return (
+        //<div className="Main">
+        // {Math.round(fahrenheit)}  
+       // <span className="Units">
+         //  <span className="Passive">
+         //  <a href="/" onClick={showCelsius}>°C</a>
+        //    </span>
+        //   <span className="Active">|°F</span>
+      //  </span>
+     //   </div> );}
