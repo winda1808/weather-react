2f91602d602d3d776437755cb938562543260a81
winda1808 <105516942+winda1808@users.noreply.github.com>
2022-07-29 00:54:55 +0800

    fixed warning

--- src/Forecast.js
+++ src/Forecast.js
@@ -30,6 +30,8 @@ export default function Forecast (props) {
                                 <ForecastDay data={dailyForecast} />
                                  </div>
                             );
+                            } else {
+                                return null;
                             }
                         })}  
                     </div>
